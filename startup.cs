ec151fabed7ed486996db105fe5d0c975219791f
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-05-25 08:50:54 -0500

    *

--- startup.cs
+++ startup.cs
@@ -53,7 +53,7 @@ namespace project
 
             services.AddCors(options =>
                 options.AddDefaultPolicy(
-                    builder => builder.WithOrigins("https://food-front-one.vercel.app").AllowAnyMethod().AllowAnyHeader().AllowCredentials())
+                    builder => builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader().AllowCredentials())
             );
             services.AddResponseCaching();
 
